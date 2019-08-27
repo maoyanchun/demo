10ec7b01d9c5625000d391b6e2139077e4b594fe
yc.mao <mycgeek@163.com>
2019-08-27 21:13:17 +0800

    test

--- src/com/micro/test/lambda/LambdaDemo.java
+++ src/com/micro/test/lambda/LambdaDemo.java
@@ -1,11 +1,20 @@
 package com.micro.test.lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mycge on 2018/7/29.
  */
 public class LambdaDemo {
     public static void main(String[] args) {
         function1();
+        function2();
+    }
+
+    private static void function2() {
+        List<Integer> list1 = new ArrayList<>();
+        //TODO
     }
 
     private static void function1() {
